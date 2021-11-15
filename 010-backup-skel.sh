15983129ee1e541fad1b8114ad681ccc90722539
fitzland <f1tzl4nd@gmail.com>
2021-11-15 07:03:27 -0600

    fix to backup script and files

--- 010-backup-skel.sh
+++ 010-backup-skel.sh
@@ -18,32 +18,22 @@ mv ./skel skel-beta
 mkdir ./skel
 cp -r $HOME/.bin ./skel/
 cp -r $HOME/.config/autostart ./skel/config/
-#cp -r $HOME/.config/bspwm ./skel/config/
-#cp -r $HOME/.config/catfish ./skel/config/
-#cp -r $HOME/.config/conky ./skel/config/
-#cp -r $HOME/.config/filezilla ./skel/config/
-#cp -r $HOME/.config/geany ./skel/config/
+cp -r $HOME/.config/bspwm ./skel/config/
 cp -r $HOME/.config/gtk-2.0 ./skel/config/
 cp -r $HOME/.config/gtk-3.0 ./skel/config/
+cp -r $HOME/.config/gtk-4.0 ./skel/config/
 cp -r $HOME/.config/i3 ./skel/config/
 cp -r $HOME/.config/keepassxc ./skel/config/
-#cp -r $HOME/.config/neofetch ./skel/config/
 cp -r $HOME/.config/nitrogen ./skel/config/
-#cp -r $HOME/.config/obmenu-generator ./skel/config/
-#cp -r $HOME/.config/openbox ./skel/config/
-#cp -r $HOME/.config/polybar ./skel/config/
 cp -r $HOME/.config/qt5ct ./skel/config/
-cp -r $HOME/.config/ristretto ./skel/config/
+cp -r $HOME/.config/regolith ./skel/config/
 cp -r $HOME/.config/rofi ./skel/config/
-#cp -r $HOME/.config/skippy-xd ./skel/config/
-cp -r $HOME/.config/spotify ./skel/config/
-#cp -r $HOME/.config/sxhkd ./skel/config/
-#cp -r $HOME/.config/termite ./skel/config/
+cp -r $HOME/.config/sxhkd ./skel/config/
 cp -r $HOME/.config/Thunar ./skel/config/
-#cp -r $HOME/.config/tint2 ./skel/config/
+cp -r $HOME/.Xresources.d ./skel/config/
 cp $HOME/.bashrc* ./skel/
-cp $HOME/.xprofile ./skel/
 cp $HOME/.Xresources ./skel/
+cp $HOME/.Xresources-regolith ./skel/
 cp $HOME/.face ./skel/
 cp $HOME/.jwmrc ./skel/
 
